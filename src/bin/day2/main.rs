6660d806b43e5ac78f77e2220e3674a9f3e13e1f
Antti Väisänen <avaisan@proton.me>
2025-12-03 22:02:00 +0200

    day 2 part 2

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -1,28 +1,26 @@
-/* Problem framing:
-"Sequence of digits repeated twice" is the key word. Our logic in code can be that we split given number in half and compare halves. If halves are the same, it is an invalid ID. If it is an odd number, it cannot be invalid.
--> Iterate through each range, check each number for invalid, gather invalids in an array, them sum up the array to get the answer.
-*/
-
 fn main() {
     let input = include_str!("../input/day2.txt");
-    let invalid_ids = invalid_ids(input);
-    println!("Invalid IDs: {:?}", invalid_ids);
-    println!("Sum of invalid IDs: {}", invalid_ids.iter().sum::<i64>());
+    
+    let invalid_ids_p1 = invalid_ids(input, is_invalid_p1);
+    println!("Part 1: Invalid IDs: {:?}", invalid_ids_p1);
+    println!("Part 1: Sum: {}", invalid_ids_p1.iter().sum::<i64>());
+    
+    let invalid_ids_p2 = invalid_ids(input, is_invalid_p2);
+    println!("Part 2: Invalid IDs: {:?}", invalid_ids_p2);
+    println!("Part 2: Sum: {}", invalid_ids_p2.iter().sum::<i64>());
 }
 
-fn invalid_ids(input: &str) -> Vec<i64> {
+fn invalid_ids(input: &str, is_invalid: fn(i64) -> bool) -> Vec<i64> {
     let mut invalid_ids = Vec::new();
     
-    // Split ranges by commas using for loop, then split ranges by dash.
     for range in input.split(',') {
-        let parts: Vec<&str> = range.split('-').collect(); // collect range into a vector parts
+        let parts: Vec<&str> = range.split('-').collect();
         let start: i64 = parts[0].parse().unwrap();
         let end: i64 = parts[1].parse().unwrap();
         
-        // Now we check each number for validity and push invalids to array
-        for num in start..=end {
-            if is_invalid(num) {
-                invalid_ids.push(num);
+        for id in start..=end {
+            if is_invalid(id) {
+                invalid_ids.push(id);
             }
         }
     }
@@ -30,7 +28,11 @@ fn invalid_ids(input: &str) -> Vec<i64> {
     invalid_ids
 }
 
-fn is_invalid(num: i64) -> bool {
+/* Part 1:
+"Sequence of digits repeated twice" is the key word. Our logic in code can be that we split given number in half and compare halves. If halves are the same, it is an invalid ID. If it is an odd number, it cannot be invalid.
+-> Iterate through each range, check each number for invalid, gather invalids in an array, them sum up the array to get the answer.
+*/
+fn is_invalid_p1(num: i64) -> bool {
     let id = num.to_string();
     
     // Easy win: odd numbers cannot repeat, so these are valid
@@ -42,6 +44,31 @@ fn is_invalid(num: i64) -> bool {
     let split = id.len() / 2;
     &id[0..split] == &id[split..]
 }
+
+/*
+Part 2: 
+"ID is invalid if it is made only of some sequence of digits repeated". So we need to check for repeating patterns, previous logic of splitting into halves, or amending logic by also checking for odd numbers, won't work.
+I will go for brute force solution. For each pattern length 1,2,3,4,..., check if by going halfway and repeating it, we get the same number. 
+E.g. 1111 -> pattern length 1, repeat 1 by pattern length 4 -> we should get original number 1111. If yes, invalid.
+1245 -> pattern length 1, repeat 1 by pattern length 4 -> we get 1111, which is not 1245 -> valid. For loop pattern length 2, get 12, repeat by pattern length 2 -> 1212, still not 1245. If for loop passes, the ID was valid.
+*/
+fn is_invalid_p2(num: i64) -> bool {
+    let id = num.to_string();
+    let length = id.len();
+    
+    for pattern_length in 1..=length/2 {
+        let pattern = &id[0..pattern_length];
+        
+        // Repeat pattern and check if it matches original
+        let repeated = pattern.repeat(length / pattern_length);
+        if repeated == id {
+            return true;
+        }
+    }
+    
+    false
+}
+
 
 #[cfg(test)]
 mod tests;
--- src/bin/day2/tests.rs
+++ src/bin/day2/tests.rs
@@ -29,47 +29,71 @@ Find all of the invalid IDs that appear in the given ranges. In the above exampl
 38593856-38593862 has one invalid ID, 38593859.
 */
 
-
+// Part 1 tests
 #[test]
 fn test_single_digit_repeated() {
-    assert_eq!(is_invalid(11), true);
-    assert_eq!(is_invalid(22), true);
-    assert_eq!(is_invalid(99), true);
+    assert_eq!(is_invalid_p1(11), true);
+    assert_eq!(is_invalid_p1(22), true);
+    assert_eq!(is_invalid_p1(99), true);
 }
 
 #[test]
 fn test_two_digit_repeated() {
-    assert_eq!(is_invalid(6464), true);
-    assert_eq!(is_invalid(1010), true);
+    assert_eq!(is_invalid_p1(6464), true);
+    assert_eq!(is_invalid_p1(1010), true);
 }
 
 #[test]
 fn test_three_digit_repeated() {
-    assert_eq!(is_invalid(123123), true);
+    assert_eq!(is_invalid_p1(123123), true);
 }
 
 #[test]
 fn test_valid_numbers() {
-    assert_eq!(is_invalid(10), false); // Halves don't match
-    assert_eq!(is_invalid(999), false);  // Odd length
-    assert_eq!(is_invalid(1234), false); // Halves don't match
+    assert_eq!(is_invalid_p1(10), false); // Halves don't match
+    assert_eq!(is_invalid_p1(999), false);  // Odd length
+    assert_eq!(is_invalid_p1(1234), false); // Halves don't match
 }
 
 #[test]
 fn test_sum_of_invalid_ids_one_invalid() {
     let input = "95-115"; // 99
-    assert_eq!(invalid_ids(input).iter().sum::<i64>(), 99);
+    assert_eq!(invalid_ids(input, is_invalid_p1).iter().sum::<i64>(), 99);
 }
 
 
 #[test]
 fn test_sum_of_invalid_ids_two_invalids() {
     let input = "11-22"; // 11 + 22 = 33
-    assert_eq!(invalid_ids(input).iter().sum::<i64>(), 33);
+    assert_eq!(invalid_ids(input, is_invalid_p1).iter().sum::<i64>(), 33);
 }
 
 #[test]
 fn test_example() {
     let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-    assert_eq!(invalid_ids(input).iter().sum::<i64>(), 1227775554);
+    assert_eq!(invalid_ids(input, is_invalid_p1).iter().sum::<i64>(), 1227775554);
+}
+
+/* Part 2 tests:
+
+These should be invalid IDs:
+111 (1 three times),
+12341234 (1234 two times), 
+123123123 (123 three times)
+
+These should be valid IDs:
+123
+1234
+ */
+#[test]
+fn test_p2_invalid_ids() {
+    assert_eq!(is_invalid_p2(111), true);       // 1 repeated 3 times
+    assert_eq!(is_invalid_p2(12341234), true);  // 1234 repeated 2 times
+    assert_eq!(is_invalid_p2(123123123), true); // 123 repeated 3 times
+}
+
+#[test]
+fn test_p2_valid_ids() {
+    assert_eq!(is_invalid_p2(123), false);  // No repeating pattern
+    assert_eq!(is_invalid_p2(1234), false); // 12 repeated twice is 1212, not 1234 -> should pass as valid
 }
